8ae67c19d63dca3f2970b98201069204e29c0fd4
Jeff Erickson <jeff@erick.so>
2018-11-11 15:27:12 -0500

    update iterm2 settings, update swe script to firefox

--- svenskt-ord.sh
+++ svenskt-ord.sh
@@ -10,7 +10,7 @@ echo -n $1 | pbcopy
 # replace Swedish vowels for antonym.se
 cleaned_word=`echo $1 | sed -e 's/[åÅ]/aa/g' -e 's/[öÖ]/oe/g' -e 's/[äÄ]/ae/g'`
 
-open -a "/Applications/Google Chrome.app" \
+open -a "/Applications/Firefox.app" \
     "https://sv.wiktionary.org/wiki/$1#Svenska" \
     "https://forvo.com/word/$1" \
     "https://www.google.se/search?site=&tbm=isch&source=hp&q=$1&oq=$1&gs_l=img.3..0l10.2298.3088.0.3176.5.5.0.0.0.0.60.218.4.4.0....0...1ac.1.64.img..1.4.216.JMP5u9OU3X8" \
