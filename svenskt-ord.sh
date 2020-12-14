156f9ad4c56d0af5ad3f7ca3f1676c9f48d8c67e
Jeff Erickson <jeff@erick.so>
2020-12-13 20:17:05 -0500

    minor: move order

--- svenskt-ord.sh
+++ svenskt-ord.sh
@@ -11,9 +11,9 @@ echo -n $1 | pbcopy
 cleaned_word=`echo $1 | sed -e 's/[åÅ]/\%C3\%A5/g' -e 's/[öÖ]/\%C3\%B6/g' -e 's/[äÄ]/\%C3\%A4/g'`
 
 open -a "/Applications/Firefox.app" \
-    "https://svenska.se/tre/?sok=$cleaned_word&pz=1" \
     "https://sv.wiktionary.org/wiki/$cleaned_word#Svenska" \
     "https://forvo.com/word/$cleaned_word" \
     "https://www.google.se/search?site=&tbm=isch&source=hp&q=$cleaned_word&oq=$cleaned_word&gs_l=img.3..0l10.2298.3088.0.3176.5.5.0.0.0.0.60.218.4.4.0....0...1ac.1.64.img..1.4.216.JMP5u9OU3X8" \
     "http://en.bab.la/dictionary/swedish-english/$cleaned_word" \
+    "https://svenska.se/tre/?sok=$cleaned_word&pz=1" \
     2>/dev/null
