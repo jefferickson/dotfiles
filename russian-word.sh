c1c8f9d2fa5f495ed576cd727b7e4ffbe48ff49e
Jeff Erickson <jeff@erick.so>
2021-01-02 21:37:11 -0500

    fix word-searching scripts

--- russian-word.sh
+++ russian-word.sh
@@ -1,14 +1,15 @@
 #! /bin/bash
 
 ## USAGE: ./russian-word.sh WORD
+## Where WORD is the Russian word you want to search.
 
 echo "Search: $1"
 
 echo -n $1 | pbcopy
 
-open -a "/Applications/Firefox.app" \
-    "https://ru.wiktionary.org/wiki/$1#Русский" \
-    "https://forvo.com/word/$1" \
-    "https://www.google.ru/search?site=&tbm=isch&source=hp&q=$1&oq=$1&gs_l=img.3..0l10.2298.3088.0.3176.5.5.0.0.0.0.60.218.4.4.0....0...1ac.1.64.img..1.4.216.JMP5u9OU3X8" \
-    "http://en.bab.la/dictionary/russian-english/$1" \
+/Applications/Firefox.app/Contents/MacOS/firefox \
+    --new-tab "https://ru.wiktionary.org/wiki/$1#Русский" \
+    --new-tab "https://forvo.com/word/$1/#ru" \
+    --new-tab "https://www.google.ru/search?site=&tbm=isch&source=hp&q=$1&oq=$1&gs_l=img.3..0l10.2298.3088.0.3176.5.5.0.0.0.0.60.218.4.4.0....0...1ac.1.64.img..1.4.216.JMP5u9OU3X8" \
+    --new-tab "http://en.bab.la/dictionary/russian-english/$1" \
     2>/dev/null
--- svenskt-ord.sh
+++ svenskt-ord.sh
@@ -1,19 +1,19 @@
 #! /bin/bash
 
 ## USAGE: ./svenskt-ord.sh ORD
-## Where ORD is the swedish word you want to search.
+## Where ORD is the Swedish word you want to search.
 
 echo "Sök: $1"
 
 echo -n $1 | pbcopy
 
-# replace Swedish vowels for antonym.se
+# replace Swedish vowels
 cleaned_word=`echo $1 | sed -e 's/[åÅ]/\%C3\%A5/g' -e 's/[öÖ]/\%C3\%B6/g' -e 's/[äÄ]/\%C3\%A4/g'`
 
-open -a "/Applications/Firefox.app" \
-    "https://sv.wiktionary.org/wiki/$cleaned_word#Svenska" \
-    "https://forvo.com/word/$cleaned_word" \
-    "https://www.google.se/search?site=&tbm=isch&source=hp&q=$cleaned_word&oq=$cleaned_word&gs_l=img.3..0l10.2298.3088.0.3176.5.5.0.0.0.0.60.218.4.4.0....0...1ac.1.64.img..1.4.216.JMP5u9OU3X8" \
-    "http://en.bab.la/dictionary/swedish-english/$cleaned_word" \
-    "https://svenska.se/tre/?sok=$cleaned_word&pz=1" \
+/Applications/Firefox.app/Contents/MacOS/firefox \
+    --new-tab "https://sv.wiktionary.org/wiki/$cleaned_word#Svenska" \
+    --new-tab "https://forvo.com/word/$cleaned_word/#sv" \
+    --new-tab "https://www.google.se/search?site=&tbm=isch&source=hp&q=$cleaned_word&oq=$cleaned_word&gs_l=img.3..0l10.2298.3088.0.3176.5.5.0.0.0.0.60.218.4.4.0....0...1ac.1.64.img..1.4.216.JMP5u9OU3X8" \
+    --new-tab "http://en.bab.la/dictionary/swedish-english/$cleaned_word" \
+    --new-tab "https://svenska.se/tre/?sok=$cleaned_word&pz=1" \
     2>/dev/null
