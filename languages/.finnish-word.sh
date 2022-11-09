09f4b6a78f1adba22bd96b4dfdd4dad4f0210b45
Jeff Erickson <jeff@erick.so>
2022-11-09 17:31:01 +0200

    fin: reorder tabs and add kieli.net

--- languages/.finnish-word.sh
+++ languages/.finnish-word.sh
@@ -12,10 +12,11 @@ echo -n $1 | pbcopy
 cleaned_word=`echo $1 | sed -e 's/[åÅ]/\%C3\%A5/g' -e 's/[öÖ]/\%C3\%B6/g' -e 's/[äÄ]/\%C3\%A4/g'`
 
 /Applications/Firefox.app/Contents/MacOS/firefox \
-    --new-tab "https://fi.wiktionary.org/wiki/$cleaned_word#Suomi" \
     --new-tab "https://forvo.com/word/$cleaned_word/#fi" \
-    --new-tab "https://www.google.fi/search?site=&tbm=isch&source=hp&q=$cleaned_word&oq=$cleaned_word&gs_l=img.3..0l10.2298.3088.0.3176.5.5.0.0.0.0.60.218.4.4.0....0...1ac.1.64.img..1.4.216.JMP5u9OU3X8" \
     --new-tab "http://en.bab.la/dictionary/finnish-english/$cleaned_word" \
+    --new-tab "https://fi.wiktionary.org/wiki/$cleaned_word#Suomi" \
+    --new-tab "https://www.google.fi/search?site=&tbm=isch&source=hp&q=$cleaned_word&oq=$cleaned_word&gs_l=img.3..0l10.2298.3088.0.3176.5.5.0.0.0.0.60.218.4.4.0....0...1ac.1.64.img..1.4.216.JMP5u9OU3X8" \
+    --new-tab "https://kieli.net/sana/$1" \
     2>/dev/null
 
 trash ~/Downloads/pronunciation_fi_*.mp3
