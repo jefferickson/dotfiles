29c356b62e669091b4754e086a7479062de17252
Jeff Erickson <jeff@erick.so>
2022-11-09 13:15:41 +0200

    auto download finnish word mp3s from forvo; display lennu frequencies

--- languages/.finnish-word.sh
+++ languages/.finnish-word.sh
@@ -16,3 +16,10 @@ cleaned_word=`echo $1 | sed -e 's/[åÅ]/\%C3\%A5/g' -e 's/[öÖ]/\%C3\%B6/g' -e
     --new-tab "https://www.google.fi/search?site=&tbm=isch&source=hp&q=$cleaned_word&oq=$cleaned_word&gs_l=img.3..0l10.2298.3088.0.3176.5.5.0.0.0.0.60.218.4.4.0....0...1ac.1.64.img..1.4.216.JMP5u9OU3X8" \
     --new-tab "http://en.bab.la/dictionary/finnish-english/$cleaned_word" \
     2>/dev/null
+
+trash ~/Downloads/pronunciation_fi_*.mp3
+curl -s https://apifree.forvo.com/action/word-pronunciations/format/json/word/$1/id_lang_speak/46/limit/1/key/$FORVO_API_KEY |
+    jq '.items[0].pathmp3' |
+    xargs -L 1 curl -s --output ~/Downloads/pronunciation_fi_$1.mp3
+
+cat ~/git-repos/lennu/output/$1
