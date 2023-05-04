409744f2048bf74d762a3dca389a58c86082c0b9
Jeff Erickson <jeff@erick.so>
2023-05-04 19:54:04 +0300

    ayu overrides

--- nvim/after/plugin/ayu.lua
+++ nvim/after/plugin/ayu.lua
@@ -1,8 +1,29 @@
+local accent = '#FFCC66'
+local customBg = '#262626'
+local customColumn = '#303030'
+local customFunc = '#FFEF7F'
+local customLineNrBg = '#1F1F1F'
+local customLineNrFg = '#4D4D4D'
+local customSelection = '#444444'
+local customSelectionBright = '#666666'
+local customWhite = '#CBCCC6'
+
 require('ayu').setup({
     mirage = true,
     overrides = {
-        Normal = { bg = '#262626' },
-        Function = { fg = '#FFEF7F' },
+        ColorColumn = { bg = customColumn },
+        CursorColumn = { bg = customColumn },
+        CursorLine = { bg = customColumn },
+        CursorLineNr = { fg = accent, bg = customLineNrBg },
+        FoldColumn = { bg = customLineNrBg },
+        Function = { fg = customFunc },
+        LineNr = { fg = customLineNrFg, bg = customLineNrBg },
+        Normal = { bg = customBg },
+        SignColumn = { bg = customColumn },
+        TabLine = { fg = customWhite, bg = customSelection },
+        TabLineFill = { bg = customSelection },
+        TabLineSel = { fg = fakeWhite, bg = customSelectionBright },
+        Visual = { bg = customSelection },
     }
 })
 
