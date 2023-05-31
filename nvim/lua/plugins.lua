6f8e47f0ad8a58a699faf2464dae8e07ba80abf9
Jeff Erickson <jeff@erick.so>
2023-05-31 11:29:27 +0300

    we may not need ctrlp after all

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -4,7 +4,6 @@ return require('packer').startup(function(use)
     use 'wbthomason/packer.nvim'
 
     use 'airblade/vim-gitgutter'
-    use 'ctrlpvim/ctrlp.vim'
     use 'ibhagwan/fzf-lua'
     use 'mechatroner/rainbow_csv'
     use 'nvim-lualine/lualine.nvim'
