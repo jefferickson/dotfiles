a1d2b34e89101ec032891e1eb648b685a4f532cc
Jeff Erickson <jeff@erick.so>
2023-05-31 12:42:29 +0300

    custom lsp autocomplete

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -1,4 +1,6 @@
 local lsp = require('lsp-zero').preset({})
+local cmp = require('cmp')
+local cmp_select_opts = {behavior = cmp.SelectBehavior.Select}
 
 lsp.ensure_installed({
 })
@@ -10,3 +12,17 @@ end)
 require('lspconfig').lua_ls.setup(lsp.nvim_lua_ls())
 
 lsp.setup()
+
+cmp.setup({
+    preselect = 'item',
+    completion = {
+        autocomplete = false,
+        completeopt = 'menu,menuone,noinsert'
+    },
+    mapping = {
+        ['<CR>'] = cmp.mapping.confirm({select = true}),
+        ['<TAB>'] = cmp.mapping.complete(),
+        ['j'] = cmp.mapping.select_next_item(cmp_select_opts),
+        ['k'] = cmp.mapping.select_prev_item(cmp_select_opts),
+    },
+})
