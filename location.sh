12f4607fbd7cb3485058365c56d3d038642f403c
Jeff Erickson <jeffrey.erickson@posti.com>
2024-10-07 15:43:37 +0300

    location: Kuopio -> Tampere

--- location.sh
+++ location.sh
@@ -5,6 +5,13 @@ CITY=$(echo "$LOCATION" | jq .city | tr -d '"')
 LON=$(echo "$LOCATION" | jq .lon)
 LAT=$(echo "$LOCATION" | jq .lat)
 
+# I'm rarely in Kuopio
+if [[ "$CITY" == "Kuopio" ]]; then
+    CITY="Tampere"
+    LON=23.7637
+    LAT=61.4980
+fi
+
 TEMP=$(curl -s "https://api.met.no/weatherapi/locationforecast/2.0/compact?lat=$LAT&lon=$LON" |
     jq '.properties.timeseries[0].data.instant.details.air_temperature')
 
