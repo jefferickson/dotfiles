12691fc03278332f3cda7d4810f0aeff0d264c73
Jeff Erickson <jeffrey.erickson@posti.com>
2024-11-05 15:04:38 +0200

    tmux: fix some locations

--- location.sh
+++ location.sh
@@ -5,11 +5,18 @@ CITY=$(echo "$LOCATION" | jq .city | tr -d '"')
 LON=$(echo "$LOCATION" | jq .lon)
 LAT=$(echo "$LOCATION" | jq .lat)
 
-# I'm rarely in Kuopio
-if [[ "$CITY" == "Kuopio" ]]; then
+# I'm more likely in Tampere than Turku or Kuopio
+if [[ "$CITY" == "Turku" ]] || [[ "$CITY" == "Kuopio" ]]; then
     CITY="Tampere"
     LON=23.7637
     LAT=61.4980
+fi
+
+# And I'm more likely in Helsinki than Vantaa
+if [[ "$CITY" == "Vantaa" ]]; then
+    CITY="Helsinki"
+    LON=24.9521
+    LAT=60.1704
 fi
 
 TEMP=$(curl -s "https://api.met.no/weatherapi/locationforecast/2.0/compact?lat=$LAT&lon=$LON" |
