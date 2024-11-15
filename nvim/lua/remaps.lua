62d680a680e1bcaa91a34a31e01a92c8b127699f
Jeff Erickson <jeffrey.erickson@posti.com>
2024-11-15 11:53:59 +0200

    nvim: add U/D for moving around file

--- nvim/lua/remaps.lua
+++ nvim/lua/remaps.lua
@@ -1,8 +1,3 @@
--- move blocks of code
-vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv")
-vim.keymap.set('v', 'K', ":m '<-2<CR>gv=gv")
-vim.keymap.set('n', 'J', 'mzJ`z')
-
 -- better cursor control
 vim.keymap.set('n', '<C-d>', '<C-d>zz')
 vim.keymap.set('n', '<C-u>', '<C-u>zz')
@@ -17,19 +12,26 @@ vim.keymap.set('n', '^', '0')
 vim.keymap.set('n', 'gg', 'gg0^')
 vim.keymap.set('n', 'G', 'G0^')
 
--- easily turn off search highlighting
-vim.keymap.set('n', '<leader><cr>', ':noh<cr>')
-
--- toggle paste mode
-vim.keymap.set('', '<leader>pp', ':setlocal paste!<cr>')
-
--- use arrow keys for nav in normal mode
+-- better file navigation
 vim.keymap.set('n', '<Up>', '<C-o>')
 vim.keymap.set('n', '<Down>', '<C-i>')
 vim.keymap.set('n', '<Left>', ':bprev<cr>')
 vim.keymap.set('n', '<Right>', ':bnext<cr>')
 vim.keymap.set('n', '<PageUp>', '<C-u>zz')
 vim.keymap.set('n', '<PageDown>', '<C-d>zz')
+vim.keymap.set('n', 'U', '<C-u>zz')
+vim.keymap.set('n', 'D', '<C-d>zz')
+
+-- move blocks of code
+vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv")
+vim.keymap.set('v', 'K', ":m '<-2<CR>gv=gv")
+vim.keymap.set('n', 'J', 'mzJ`z')
+
+-- easily turn off search highlighting
+vim.keymap.set('n', '<leader><cr>', ':noh<cr>')
+
+-- toggle paste mode
+vim.keymap.set('', '<leader>pp', ':setlocal paste!<cr>')
 
 -- better integration with karabiner mods
 -- shift becomes z<cr>
